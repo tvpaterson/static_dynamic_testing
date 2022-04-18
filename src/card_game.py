23534be3c47ea9f46ea31953b270f43aad70ed31
tvpaterson <taylorpaterson7@gmail.com>
2022-04-18 12:54:39 +0100

    added screenshot folders

--- src/card_game.py
+++ src/card_game.py
@@ -25,3 +25,29 @@ class CardGame:
     for card in cards:
       total += card.value
     return f"You have a total of {total}"
+
+
+# class CardGame:
+
+
+#   def check_for_ace(self, card):
+#     if card.value = 1:
+#       return True
+#     else
+#       return False
+   
+
+#   dif highest_card(self, card1 card2):
+#   if card1.value > card2.value:
+#     return card
+#   else:
+#     return card2
+  
+
+
+# def cards_total(self, cards):
+#   total
+#   for card in cards:
+#     total += card.value
+#     return "You have a total of" + total
+  
